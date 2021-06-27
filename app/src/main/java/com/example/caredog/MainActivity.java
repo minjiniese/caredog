32328606e0c2460d523347c6a41e39e287f4c488
minjinkim <kmj101012@naver.com>
2021-06-27 16:32:49 +0900

    test
    test 창준

--- app/src/main/java/com/example/caredog/MainActivity.java
+++ app/src/main/java/com/example/caredog/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //지혜 ㅎㅇ
+        //창준오빠 ㅎㅇ
     }
 }
