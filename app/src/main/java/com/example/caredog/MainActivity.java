8cdd88209d84e79c7264e5eac529584762b010b2
Changjooon <skrymir94@gmail.com>
2021-06-28 12:43:36 +0900

    오오
    오오오오오오

--- app/src/main/java/com/example/caredog/MainActivity.java
+++ app/src/main/java/com/example/caredog/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //창준오빠 ㅎㅇ
+        //ㅎㅇㅎㅇ
     }
 }
