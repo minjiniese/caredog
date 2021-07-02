299bc030b28805f55080c036d229af0ebf44d325
minjinkim <kmj101012@naver.com>
2021-07-02 18:41:00 +0900

    메인 레이아웃
    메인 레이아웃

--- app/src/main/java/com/example/caredog/MainActivity.java
+++ app/src/main/java/com/example/caredog/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.caredog;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+
+        //메인 화면으로 가기 버튼~
+        Button mainButton = (Button) findViewById(R.id.button);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
