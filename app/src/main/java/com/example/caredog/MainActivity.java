75fefa997267d07c10982ffcc42bbecd4772edc1
jihye <26017097@naver.com>
2021-07-05 23:02:39 +0900

    MainActivity.java, SubActivity.java
    일부 오류 수정

--- app/src/main/java/com/example/caredog/MainActivity.java
+++ app/src/main/java/com/example/caredog/MainActivity.java
@@ -1,15 +1,29 @@
-package com.example.caredog;
+package com.example.caredog_test;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.caredog.SubActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //지혜 ㅎㅇ
-        //여기서 바꾸면 언니한테도 그렇게 떠?
+        setContentView(R.layout.activity_sign_user);
+
+        //다음 버튼을 누르면 회원가입_반려견 화면으로 이동
+        Button sign_user_next = (Button) findViewById(R.id.sign_user_next);
+        sign_user_next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
