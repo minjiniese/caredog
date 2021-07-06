6f9d344d1225c13975b9a4df25d2ece9f04a55c7
altmen77 <68007427+altmen77@users.noreply.github.com>
2021-07-06 15:22:24 +0900

    Merge remote-tracking branch 'origin/minjin' into hyunwoo
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/caredog/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/caredog/dataActivity.java
+++ app/src/main/java/com/example/caredog/dataActivity.java
@@ -11,28 +11,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class settingActivity extends AppCompatActivity {
-    ArrayList<String> list;
-
+public class dataActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
-        list = new ArrayList<>();
-        list.add("아이템 1");
-        list.add("아이템 2");
-        list.add("아이템 3");
-        list.add("아이템 4");
-        list.add("아이템 5");
-        list.add("아이템 6");
-        Adapter adapter = new Adapter(list);
-        RecyclerView recyclerView = findViewById(R.id.list_food);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+        setContentView(R.layout.activity_data);
 
         //뒤로가기 버튼을 누르면 메인 화면으로 이동
-        ImageButton setting_back = (ImageButton) findViewById(R.id.setting_back);
-        setting_back.setOnClickListener(new View.OnClickListener() {
+        ImageButton data_back = (ImageButton) findViewById(R.id.data_back);
+        data_back.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
@@ -42,4 +29,3 @@ public class settingActivity extends AppCompatActivity {
         });
     }
 }
-
