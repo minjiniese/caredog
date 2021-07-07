152d8154baba77cbde6055080a3371331819a763
altmen77 <68007427+altmen77@users.noreply.github.com>
2021-07-07 13:51:39 +0900

    Merge remote-tracking branch 'origin/minjin' into hyunwoo
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/caredog/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/caredog/timepickerActivity.java
+++ app/src/main/java/com/example/caredog/timepickerActivity.java
@@ -1,7 +1,10 @@
 package com.example.caredog;
 
+import android.app.TimePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
@@ -10,21 +13,57 @@ import androidx.appcompat.app.AppCompatActivity;
 //setting에서 추가버튼 누르면 나올 시계 화면
 public class timepickerActivity extends AppCompatActivity {
 
+    private TextView textView_Date;
+    private TimePickerDialog.OnTimeSetListener callbackMethod;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timepicker);
+        this.InitializeView();
+        this.InitializeListener();
 
-        TimePicker mTimePicker = (TimePicker) findViewById(R.id.timepicker);
-        int hour, min;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            hour = mTimePicker.getHour();
-            min = mTimePicker.getMinute();
-        } else {
-            hour = mTimePicker.getCurrentHour();
-            min = mTimePicker.getCurrentMinute();
-        }
+        textView_Date = (TextView)findViewById(R.id.textvieww);
     }
 
+    public void InitializeView()
+    {
+        textView_Date = (TextView)findViewById(R.id.textvieww);
+    }
+
+    public void InitializeListener()
+    {
+        callbackMethod = new TimePickerDialog.OnTimeSetListener()
+        {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+            {
+                textView_Date.setText(hourOfDay + "시" + minute + "분");
+            }
+        };
+    }
+    public void OnClickHandler(View view)
+    {
+        TimePickerDialog dialog = new TimePickerDialog(this, callbackMethod, 8, 10, true);
+
+        dialog.show();
+    }
 }
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.timepicker);
+//
+//        TimePicker mTimePicker = (TimePicker) findViewById(R.id.timepicker);
+//        int hour, min;
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            hour = mTimePicker.getHour();
+//            min = mTimePicker.getMinute();
+//        } else {
+//            hour = mTimePicker.getCurrentHour();
+//            min = mTimePicker.getCurrentMinute();
+//        }
+//    }
+//
+//}
 
