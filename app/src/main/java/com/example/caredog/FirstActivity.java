4f9ffa22ea93242810a756a7492d3c1190f2a8aa
Changjooon <skrymir94@gmail.com>
2021-07-12 20:31:43 +0900

    FirstActivity, LoginActivity
    add FirstActivity, LoginActivity

---
+++ app/src/main/java/com/example/caredog/FirstActivity.java
@@ -0,0 +1,28 @@
+package com.example.caredog;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        //로그인화면으로 가는버튼
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/caredog/GooglemapActivity.java
@@ -0,0 +1,47 @@
+package com.example.caredog;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+//implements OnMapReadyCallback 구글맵 준비되면 호출
+public class GooglemapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;//전역변수 설정
+    private MapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_googlemap);
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    //구글맵 준비되면 호출
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(37.392103, 126.919674);//괄호안에 호출받은 위도경도값 (현재는 안양대 위치)
+        MarkerOptions markerOptions = new MarkerOptions();//마커 생성
+        markerOptions.title("현위치");//마커 이름
+        //markerOptions.snippet("")//세부내용 적을수 있음
+        markerOptions.position(location);//위의 위도경도값 갖고옴
+        googleMap.addMarker(markerOptions);//셋팅한 마커옵션 추가
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));//숫자 키울수록 확대해서 보임
+        //googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 16));//애니메이션효과
+
+    }
+}
