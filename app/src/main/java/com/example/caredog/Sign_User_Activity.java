9258b89827fcc0e830dfbf8a601146cdf0b7dbcf
jihye <26017097@naver.com>
2021-07-13 14:40:34 +0900

    수정 및 추가
    MainActivity -> Sign_User_Activity
    SubActivity -> Sign_Dog_Activity
    calendar_image 추가
    activity_sign_dog.xml 수정

---
+++ app/src/main/java/com/example/caredog/Sign_User_Activity.java
@@ -0,0 +1,28 @@
+package com.example.caredog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Sign_User_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_user);
+
+        //다음 버튼을 누르면 회원가입_반려견 화면으로 이동
+        Button sign_user_next = (Button) findViewById(R.id.sign_user_next);
+        sign_user_next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
